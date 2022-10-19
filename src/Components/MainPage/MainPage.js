15f8525eb09852358791ae8db1f50e250f2be9fd
mari-trancoso <mnegraotrancoso@gmail.com>
2022-10-19 08:51:13 -0300

    formulário da tarde

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -7,6 +7,7 @@ const MainPage = () => {
 const [name, setName] = useState("")
 const [age, setAge] = useState("")
 const [email, setEmail] = useState("")
+const [confirmaEmail, setConfirmaEmail] = useState("")
 const [formFlow, setFormFlow] = useState(1) //você não precisa mexer neste estado, ele faz parte da lógica da linha 30 do JSX
 
 const onChangeName = (event) => {
@@ -21,15 +22,39 @@ const onChangeEmail = (event) => {
   setEmail(event.target.value)
 }
 
-const sendData = () => {
+const onChangeConfirmaEmail = (event) => {
+  setConfirmaEmail(event.target.value)
+}
+
+const sendData = (event) => {
   //aqui deve vir uma verificação para mudar de formulario apenas se todos os requisitos tiverem sido cumpridos
-  setFormFlow(2)
+  if (age >=18 && name.length<=30 && name.length>=10 && email.includes("@" && ".com") && email===confirmaEmail && name!=("") && email!=("") && confirmaEmail!=("") && age!=("")){
+    setName("")
+    setAge("")
+    setEmail("")
+    setFormFlow(2)
+  } else {
+    alert("dados preenchidos incorretamente")
+  }
+  
 }
   return (
     <MainContainer>
       <h2>Formulário de inscrição</h2>
       {formFlow === 1 ? <NameForm
       // insira aqui suas props
+      name={name}
+      age={age}
+      email={email}
+      confirmaEmail={confirmaEmail}
+      onChangeAge={onChangeAge}
+      onChangeName={onChangeName}
+      onChangeEmail={onChangeEmail}
+      onChangeConfirmaEmail={onChangeConfirmaEmail}
+      setName={setName}
+      setAge={setAge}
+      setEmail={setEmail}
+      sendData={sendData}
       /> : <ConfirmationForm />}
     </MainContainer>
   )
